b3f91fc8e17ba9c05fb901f14f6d72ee550025ad
Darshil <darshil.s@squareinfosoft.com>
2022-02-14 01:29:54 +0530

    intial setup

---
+++ lib/screens/set_location.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SetLocation extends StatefulWidget {
+  const SetLocation({Key? key}) : super(key: key);
+
+  @override
+  _SetLocationState createState() => _SetLocationState();
+}
+
+class _SetLocationState extends State<SetLocation> {
+  int color = 0xFF5663FF;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 40),
+              child: Text(
+                'Hi Jeet,\nWelcome to \nGathrr!',
+                style: GoogleFonts.josefinSans(
+                  textStyle: const TextStyle(
+                    fontSize: 80,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF5663FF)
+                  )
+                )
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 40, top: 60),
+              child: Text(
+                  'Please turn on your GPS to find out better events suggestions\nnear you.',
+                  style: GoogleFonts.josefinSans(
+                      textStyle: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w300,
+                          color: Color(color)
+                      )
+                  )
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 36, right: 36, top: 60, bottom: 30),
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Color(color),
+              ),
+              child: Text(
+                  'Turn On GPS',
+                  style: GoogleFonts.josefinSans(
+                      textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFFFFFFFF)
+                      )
+                  )
+              ),
+            ),
+          ],
+        )));
+  }
+}
